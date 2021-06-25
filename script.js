f09353385915c404fea1e1c6c50b8cc7da55fbe2
samwed78 <84482279+samwed78@users.noreply.github.com>
2021-06-24 23:38:12 -0500

    completed assignment 5

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function displayMission () {
    fetch('https://handlers.education.launchcode.org/static/planets.json').then((response) => {
       response.json().then((json) => {
-         let destination = document.getElementById('missionTarget');
+         const destination = document.getElementById('missionTarget');
          let index = Math.floor(Math.random() * 10);
          while (index >= json.length) {
             index = Math.floor(Math.random() * 10);
@@ -12,7 +12,7 @@ function displayMission () {
          let html = `<h2>Mission Destination</h2>
                         <ol>  
                            <li>Name: ${json[index].name}</li>
-                           <li>Diameter: ${jason[index].diameter}</li>
+                           <li>Diameter: ${json[index].diameter}</li>
                            <li>Star: ${json[index].star}</li>
                            <li>Distance from Earth: ${json[index].distance}</li>
                            <li>Number of Moons: ${json[index].moons}</li>
@@ -27,43 +27,14 @@ function displayMission () {
    });
 }
 
-
-
-function validateForms(event, inputs) {
-   tripRequirements(inputs);
-if (inputs.every((item) => item.value !== "" )) {   
-   event.preventDefault();
-}
-else {
-   alert('All fields are required');
-   event.preventDefault();
-}
-if (typeof inputs[0].value !== "string" || typeof inputs[1].value !== "string") {
-   alert('Please enter a valid name');
-   event.preventDefault(); 
-}
-if (isNaN(Number(inputs[2].value)) || isNaN(Number(inputs[3].value))) {
-   alert('Please enter a valid number');
-   event.preventDefault();
-   }
-}
-
-function tripRequirements(inputs) {
-   let display = document.getElementById('faultyItems');
-   let h2status = document.getElementById(launchStatus);
-   let liPilot = document.getElementById(pilotStatus);
-   let liCoPilot = document.getElementById(copilotStatus);
-   let liFuel = document.getElementById(fuelStatus);
-   let liMass = document.getElementById(cargoStatus);
-
-      if (display && h2Status && liPilot && liCoPilot && liFuel & liMass) {
-         h2status.innerHTML = 'Shuttle is ready for launch.';
-         h2status.style.color = 'Green';
-         display.style.visibility = 'Hidden';
-         liPilot.innerHTML = `Pilot ${inputs[0].value} is ready for launch.`;
-         liCoPilot.innerHTML = `CoPilot ${inputs[1].value} is ready for launch`;
-         liFuel.innerHTML = 'Fuel level is accurate for launch';
-         liMass.innerHTML = "Cargo mass is accurate for launch";
+      // if (display && h2Status && liPilot && liCoPilot && liFuel & liMass) {
+      //    h2status.innerHTML = 'Shuttle is ready for launch.';
+      //    h2status.style.color = 'Green';
+      //    display.style.visibility = 'Hidden';
+      //    liPilot.innerHTML = `Pilot ${inputs[0].value} is ready for launch.`;
+      //    liCoPilot.innerHTML = `CoPilot ${inputs[1].value} is ready for launch`;
+      //    liFuel.innerHTML = 'Fuel level is accurate for launch';
+      //    liMass.innerHTML = "Cargo mass is accurate for launch";
 
          // if (inputs.every((item) => item.value !== "")) {
          // }
@@ -85,25 +56,61 @@ function tripRequirements(inputs) {
          // }
 
 
+function validateForm(event, inputs) {
+   tripRequirements(inputs);
+if (inputs.every((item) => item.value !== "" )) {   
+   event.preventDefault();
+}
+else {
+   alert('All fields are required');
+   event.preventDefault();
+}
+if (typeof inputs[0].value !== "string" || typeof inputs[1].value !== "string") {
+   alert('Please enter a valid name');
+   event.preventDefault(); 
+}
+if (isNaN(Number(inputs[2].value)) || isNaN(Number(inputs[3].value))) {
+   alert('Please enter a valid number');
+   event.preventDefault();
+   }
+}
+
+function tripRequirements(inputs) {
+   const display = document.getElementById('faultyItems');
+   const h2status = document.getElementById('launchStatus');
+   const liPilot = document.getElementById('pilotStatus');
+   const liCoPilot = document.getElementById('copilotStatus');
+   const liFuel = document.getElementById('fuelStatus');
+   const liMass = document.getElementById('cargoStatus');
+
          if (Number(inputs[2].value) < 10000 && Number(inputs[3].value) > 10000) {
-            display.style.visibility = 'Visible';
+            display.style.visibility = 'visible';
             h2status.innerHTML = 'Shuttle not ready for launch';
             h2status.style.color = 'Red'
             liFuel.innerHTML = 'Fuel level too low for launch';
             liMass.innerHTML = 'Cargo mass too high for launch';
             
-         }
-            else if (Number(inputs[2].value) >= 10000 && Number(inputs[3].value) > 10000) {
-               display.value.visibility = 'Visible'
+         } 
+          else if (Number(inputs[2].value) < 10000 && Number(inputs[3].value) < 10000) {
+               display.style.visibility = 'visible'
+               h2status.innerHTML = 'Shuttle not ready for launch';
+               h2status.style.color = 'Red';
+               liFuel.innerHTML = 'Fuel level too low for launch';
+               liMass.innerHTML = 'Shuttle mass low enough for launch';
+
+            }
+            else if  (Number(inputs[2].value) >= 10000 && Number(inputs[3].value) > 10000) {
+               display.style.visibility = 'visible'
                h2status.innerHTML = 'Shuttle not ready for launch';
                h2status.style.color = 'Red';
                liFuel.innerHTML = 'Fuel level high enough for launch';
-               liMass.innerHTML = 'Shuttle mass too heavy for launch';
+               liMass.innerHTML = 'Shuttle mass too high for launch';
 
             }
             else {
+               h2status.innerHTML = 'Shuttle is ready for launch.';
                h2status.style.color = 'Green';
-               display.style.vesibility = 'Visible';
+               display.style.visibility = 'visible';
                liPilot.innerHTML  = `Pilot ${inputs[0].value} is ready for launch`;
                liCoPilot.innerHTML = `CoPilot ${inputs[1].value} is ready for launch`;
                liFuel.innerHTML = 'Fuel level is high enough for launch';
@@ -113,15 +120,12 @@ function tripRequirements(inputs) {
 
       }
 
-}
-
-
 function onSubmit() {
-   let submit = document.getElementById('formSubmit');
-   let inputs = Array.from(document.getElementsByTagName('input'));
+   const submit = document.getElementById('formSubmit');
+   const inputs = Array.from(document.getElementsByTagName('input'));
    if (submit) {
       submit.addEventListener('click', (event) => {
-         validateForms(event, inputs);
+         validateForm(event, inputs);
          tripRequirements(inputs);
       });
    }
@@ -137,6 +141,5 @@ function genesis () {
 
 window.onload = genesis; 
 
-// window.addEventListener(load, function() {
-//    form.addEventListener("submit", function(event) 
+
 
